c864281f9f7bb6225122ed5fa31f1a40a5f48972
Moui <131141843+M0uidev@users.noreply.github.com>
2025-11-04 17:14:33 -0300

    Add pagina1 retail product grid layout

--- proyectoOnichan/appOnichan/views.py
+++ proyectoOnichan/appOnichan/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
+
 def index(request):
-    return render(request, 'pagina1.html')
+    return render(request, "pagina1.html")
+
 
 def pagina1(request):
-    return render(request, 'pagina1.html')
+    return render(request, "pagina1.html")
+
 
 def pagina2(request):
-    return render(request, 'pagina2.html')
+    return render(request, "pagina2.html")
+
 
 def pagina3(request):
-    return render(request, 'pagina3.html')
+    return render(request, "pagina3.html")
