ccc9e48639c8bda6c9de9dfeb4641b352852dd89
M0uidev <m0ui.devv@gmail.com>
2025-11-05 04:23:37 -0300

    Commit final

--- proyectoOnichan/appOnichan/urls.py
+++ proyectoOnichan/appOnichan/urls.py
@@ -4,7 +4,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('pagina1/', views.pagina1, name='pagina1'),
     path('iniciosesionadmin/', views.iniciosesionadmin, name='iniciosesionadmin'),
-    path('pagina3/', views.pagina3, name='pagina3'),
+    path('adminstock/', views.pagina3, name='pagina3'),
     path('dashboardadmin/', views.dashboardadmin, name='dashboardadmin'),
     path("producto/<int:pid>/", views.producto_detalle, name="producto_detalle"),
 ]
